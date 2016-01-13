cab3c354e782ad066d35a95abdc77208245dd65e
txoof <aaron.ciuffo@gmail.com>
2016-01-13 12:37:24 +0100

    thingiverse_prep.sh
    create a thingiverse publishable version

---
+++ thingiverse_prep.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cat tSlot_box_thingiverse_header.scad > tSlot_box_thingiverse.scad
+cat nuts_and_bolts.scad >> tSlot_box_thingiverse.scad
+cat tSlot_box.scad >> tSlot_box_thingiverse.scad
